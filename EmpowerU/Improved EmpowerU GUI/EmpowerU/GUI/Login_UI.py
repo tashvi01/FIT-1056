d6ff5a82769e8611900ac6ab1f2047a35df5129d
Tin24433 <tintran24433@gmail.com>
2024-10-01 23:05:34 +1000

    Update Login_UI.py

--- EmpowerU/Improved EmpowerU GUI/EmpowerU/GUI/Login_UI.py
+++ EmpowerU/Improved EmpowerU GUI/EmpowerU/GUI/Login_UI.py
@@ -4,26 +4,27 @@ from GUI.Register import Register
 class LoginScreen(tk.Frame):
     def __init__(self, master):
         super().__init__(master=master)
+        super().configure(bg="navy")
         self.master = master
 
         # Load logo image
         self.logo = tk.PhotoImage(file="./GUI/logo.png")  
-        self.label = tk.Label(master=self, image=self.logo, width=750, height=300)  
+        self.label = tk.Label(master=self, image=self.logo, width=750, height=300, bg="navy")  
         self.label.grid(row=0, column=0, sticky=tk.S, padx=10, pady=10) 
 
         # Welcome heading
-        self.welcome_label = tk.Label(master=self, text="Welcome to EmpowerU", font=("Arial Bold", 20))
+        self.welcome_label = tk.Label(master=self, text="Welcome to EmpowerU", font=("Arial Bold", 20), bg="navy", fg="white")
         self.welcome_label.grid(row=1, column=0, pady=10)
 
         # Create buttons below the welcome label
-        self.register_button = tk.Button(master=self, text="Register", command = self.open_register)
+        self.register_button = tk.Button(master=self, text="Register", command = self.open_register, bg="deepskyblue", fg="black")
         self.register_button.grid(row=2, column=0, padx=10, pady=10)
 
-        self.signin_button = tk.Button(master=self, text="Sign In")
+        self.signin_button = tk.Button(master=self, text="Sign In", bg="deepskyblue", fg="black")
         self.signin_button.grid(row=3, column=0, padx=10, pady=10)
 
         # Button to shut down
-        self.shutdown_button = tk.Button(master=self, text="Shut down", command=master.destroy)
+        self.shutdown_button = tk.Button(master=self, text="Shut down", command=master.destroy, bg="deepskyblue", fg="black")
         self.shutdown_button.grid(row=5, column=0, padx=10, pady=10)
 
     def open_register(self):
