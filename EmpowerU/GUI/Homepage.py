3b542d965369a24c65bf31ecb470dd5976b2f8c9
TommyNguyen21 <ntm.tom@gmail.com>
2024-10-02 19:57:45 +1000

    Update Homepage.py

--- EmpowerU/GUI/Homepage.py
+++ EmpowerU/GUI/Homepage.py
@@ -8,6 +8,7 @@ import tkinter as tk
 class Homepage(tk.Frame):
     def __init__(self,master):
         super().__init__(master)
+        self.master = master
         self.fullscreen(master)
         
         
@@ -15,13 +16,13 @@ class Homepage(tk.Frame):
         #background colour learnt from https://www.geeksforgeeks.org/tkinter-colors/
         self.config(bg="lightblue")
         
-        self.logo = tk.PhotoImage(file="logo.png")
+        
 
         self.top_bar = tk.Frame(self.master, bg="steelblue")  # Specify the background color and height
         self.top_bar.grid(row=0, column=0, sticky="new")  # Place it in the first row and expand it east-west
 
         
-
+        #TOP BAR
         #Home label
         # fotnsize https://www.geeksforgeeks.org/how-to-change-the-tkinter-label-font-size/
         self.home_label = tk.Label(self.top_bar, text="HOME", bg="steelblue",font = "Impact 20")
@@ -29,10 +30,10 @@ class Homepage(tk.Frame):
 
         #Progress bar
         #TODO: progress bar
-        self.progressbar_filler = tk.Label(self.top_bar,text = "PROGRESS BAR PLACEHOLDER",font = "Impact 20")
+        self.progressbar_filler = tk.Label(self.top_bar,text = "PROGRESS BAR PLACEHOLDER",font = "Impact 20", bg="steelblue")
         self.progressbar_filler.grid(row=0,column=1,padx=20,pady=20)
 
-        self.profile_button = tk.Button(self.top_bar,text="PROFILE",font = "Impact 20")
+        self.profile_button = tk.Button(self.top_bar,text="PROFILE",font = "Impact 20", bg="mediumspringgreen")
         self.profile_button.grid(row=0,column=2,padx=20,pady=20)
 
         #configures the columns for the top bar
@@ -40,14 +41,24 @@ class Homepage(tk.Frame):
         self.top_bar.grid_columnconfigure(1, weight=1)
         self.top_bar.grid_columnconfigure(2, weight=1)
 
+
+        #BOTTOM HALF OF SCREEN
         self.bot_screen = tk.Frame(self.master, bg = "lightblue")
         self.bot_screen.grid(row=1,column=0,columnspan=3,sticky="nsew")
 
-        for i in [0,1]:
-            self.bot_screen.grid_rowconfigure(i, weight=1)
+        # Configure rows and columns for even distribution
+        self.bot_screen.grid_rowconfigure(0, weight=1)
+        self.bot_screen.grid_rowconfigure(1, weight=1)  
+        self.bot_screen.grid_rowconfigure(2, weight=1)
 
-        for i in [0,1,2]:
-            self.bot_screen.grid_columnconfigure(i, weight=1)
+        self.bot_screen.grid_columnconfigure(0, weight=1)
+        self.bot_screen.grid_columnconfigure(1, weight=1)  
+        self.bot_screen.grid_columnconfigure(2, weight=1)
+            
+
+        self.logo = tk.PhotoImage(file="logo.png")
+        self.logo_label = tk.Label(self.bot_screen,image=self.logo)
+        self.logo_label.grid(row = 1 , column=2)
         
 
 
